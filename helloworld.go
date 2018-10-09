01bb6fcf4b7ffafd316dc213f13ddbe93bd67db0
Daniel Stancu <dstancu@gopro.com>
2018-10-09 15:51:30 +0300

    Adding go code for helloworld app.

---
+++ helloworld.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "log"
+  "net/http"
+  "os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  log.Print("Hello world received a request.")
+  target := "Knative"
+  if target == "" {
+    target = "World"
+  }
+  fmt.Fprintf(w, "Hello %s!\n", target)
+}
+
+func main() {
+  flag.Parse()
+  log.Print("Hello world sample started.")
+
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+}
+
